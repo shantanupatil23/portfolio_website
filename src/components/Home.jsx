b95498c31c6fc06f89e176d4542ee4e3f35c36ab
shantanupatil23 <kanhapatildurg@gmail.com>
2021-03-03 02:52:08 +0530

    update home

---
+++ src/components/Home.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import img_browser from "../assets/img_browser.png";
+import img_phone from "../assets/img_phone.png";
+import img_aurora from "../assets/img_aurora.jpg";
+
+function Home() {
+  return (
+    <section
+      className="section"
+      style={{
+        backgroundImage: `url(${img_aurora})`,
+      }}
+    >
+      <h1 className="intro_text">
+        Hi,
+        <br />
+        I'm Shantanu
+        <br />
+        an App Developer.
+      </h1>
+      <img className="img_browser" src={img_browser} alt="browser" />
+      <img className="img_phone" src={img_phone}  alt="phone" />
+    </section>
+  );
+}
+
+export default Home;
