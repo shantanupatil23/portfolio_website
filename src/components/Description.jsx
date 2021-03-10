afc0394cd1aa7bd6d4b748c4e2cd6af2f8099b53
shantanupatil23 <kanhapatildurg@gmail.com>
2021-03-11 00:31:39 +0530

    update description

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -8,7 +8,7 @@ function Description() {
       <img className="img_shantanu" src={img_shantanu} alt="shantanu" />
       <h1 className="name_text">Shantanu Patil</h1>
       <p className="description_text">
-      A pre-final year CS student and a freelancer currently living in Pune. Having 3 years of experience as a Native Android Developer or a Flutter Developer. Recently, I've stepped onto the track of full-stack Web Development and I'm going one step ahead every single day.
+      A pre-final year CS student, a developer and a freelancer currently living in Pune. Having 3 years of experience as a Native Android Developer or a Flutter Developer. Recently, I've stepped onto the track of full-stack Web Development and I'm going one step ahead every single day.
       </p>
       <img className="img_browser_description" src={img_browser} alt="browser" />
     </section>
