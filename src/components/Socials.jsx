e6d23bf31774bcacae3d4d8cde8de744b9169558
shantanupatil23 <kanhapatildurg@gmail.com>
2021-03-12 12:29:41 +0530

    update socials section

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -1,21 +1,42 @@
 import React from "react";
+import img_wheel from "../assets/socials/img_wheel.png";
+import img_cube from "../assets/socials/img_cube.png";
 
 function Socials() {
   return (
     <section id="socials">
+      <img className="img_cube socialsBg" src={img_cube} alt="img_cube" />
+      <img className="img_wheel socialsBg" src={img_wheel} alt="img_wheel" />
+      <p className="socialsBg text-keyboard">66.2 wpm</p>
+      <i class="far fa-keyboard socialsBg fa-7x"></i>
       <p className="socials_text">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non elit
-        id tortor interdum convallis. Morbi commodo sed sapien non viverra. Nunc
-        non cursus leo. Pellentesque habitant morbi tristique senectus et netus
-        et malesuada fames ac turpis egestas. Aliquam vitae magna vel ante
-        auctor faucibus quis ac est. Vestibulum ac dui tempor, suscipit ipsum
-        in, vulputate lacus. Curabitur eros odio.
+        Are you a Developer?
+        <br />
+        Do you feel alive while being on a virtual race track?
+        <br />
+        Can you solve the Rubik's cube in under 25 seconds or ever wanted to?
+        <br />
+        Ti piace esplorare la lingua italiana come me?
+        <br />
+        Do you like to leave graphite on paper in the form of art?
+        <br />
+        Are you an investor? Let's talk about stocks.
+        <br />
+        Let's have a talk!
       </p>
       <div className="row">
-        <i className="fab fa-linkedin socials_icon fa-7x"></i>
-        <i className="fab fa-github socials_icon fa-7x"></i>
-        <i className="fab fa-hackerrank socials_icon fa-7x"></i>
-        <i className="fab fa-instagram socials_icon fa-7x"></i>
+        <a href="https://www.linkedin.com/in/shantanupatil23/">
+          <i className="fab fa-linkedin socials_icon fa-4x"></i>
+        </a>
+        <a href="https://github.com/shantanupatil23">
+          <i className="fab fa-github socials_icon fa-4x"></i>
+        </a>
+        <a href="https://www.hackerrank.com/shantanu447">
+          <i className="fab fa-hackerrank socials_icon fa-4x"></i>
+        </a>
+        <a href="https://www.instagram.com/shantanu.patil_/">
+          <i className="fab fa-instagram socials_icon fa-4x"></i>
+        </a>
       </div>
     </section>
   );
